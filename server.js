3ddd5e36a955c2130842e8a78240efcddc0ff3cc
CalClemmer <cal1296@yahoo.com>
2021-09-09 15:13:50 -0700

    Adding delete and add functions for inventory

--- server.js
+++ server.js
@@ -133,8 +133,22 @@ app.post('/addingredient', async function(req, res) {
   //console.log('Doggo?', req.body.ingredient);
   let ingredientId = req.body.ingredient
 
-  const addIngredient = await Inventory.create({ id, ingredientId });
+  const addIngredient = await Inventory.create({ userId: id, ingredientId });
   console.log(addIngredient.toJSON());
+
+  res.redirect('/');
+});
+
+app.post('/deleteingredient', async function(req, res) {
+
+  const { id } = req.user.get(); 
+  //console.log('LOL LOOK HERE', req.body);
+  //These both work fantastically for grabbing the form's input data 
+  //console.log('Doggo?', req.body.ingredient);
+  let ingredientId = req.body.ingredient
+
+  const delIngredient = await Inventory.delete({ userId: id, ingredientId });
+  console.log(delIngredient.toJSON());
 
   res.redirect('/');
 });
